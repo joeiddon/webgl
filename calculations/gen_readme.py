0340de9262f348840fb31e7b7872a0ffe5957358
joeiddon <joedominiciddon@gmail.com>
2019-08-04 22:38:56 +0100

    calculations gen_readme.py fixed

--- calculations/gen_readme.py
+++ calculations/gen_readme.py
@@ -1,7 +1,7 @@
 import sys, os
 
 with open('README.md','w') as fh:
-    fh.write('Generate this readme with `python gen_readme.py` (run in `/calculations` directory).\n')
+    fh.write('Generate this readme with `python gen_readme.py` (run in `/calculations` directory).\n\n')
     for f in os.listdir():
-        if not sys.argv[0].endswith(f):
-            fh.write(f'[{f}](https://joeiddon.github.io/webgl/calculations/{f})\n')
+        if not sys.argv[0].endswith(f) and f != 'README.md':
+            fh.write(f'- [{f}](https://joeiddon.github.io/webgl/calculations/{f})\n')
